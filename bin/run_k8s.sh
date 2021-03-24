c5f77f8d6afcb677bd028d81f3b51a5779c13ccc
Guillermo Ampie <ga.ampie@gmail.com>
2021-03-24 16:14:42 -0600

    Use extended output in commands [skip ci]

--- bin/run_k8s.sh
+++ bin/run_k8s.sh
@@ -19,13 +19,13 @@ kubectl create deployment ${DEPLOYMENT_NAME} --image=${dockerpath} --replicas=${
 # List kubernetes resources
 echo
 echo "Listing deployments"
-kubectl get deployments
+kubectl get deployments -o wide
 echo
 echo "Listing services"
-kubectl get services
+kubectl get services -o wide
 echo
 echo "Listing pods"
-kubectl get pods
+kubectl get pods -o wide
 
 # Forward the container port to a host port
 #kubectl port-forward service/${DEPLOYMENT_NAME} ${HOST_PORT}:${CONTAINER_PORT}
