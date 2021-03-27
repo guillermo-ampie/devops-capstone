507e91d0a8835a7a6ff157a2d1b8b6398bd549e2
Guillermo Ampie <ga.ampie@gmail.com>
2021-03-27 13:59:54 -0600

    Update app version

--- hello_app/hello.py
+++ hello_app/hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World [ 2 ]")
+    name = request.args.get("name", "World [ 3 ]")
     return f'Hello, {escape(name)}!'
 
 
