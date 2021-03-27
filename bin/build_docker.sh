e91fafb9fc58fa004d7ca37589c7529fe90832d7
Guillermo Ampie <ga.ampie@gmail.com>
2021-03-27 12:10:25 -0600

    Update version format for container image

--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME="hello-app"
-VERSION=1.0
+VERSION=1.000
 
 # Step 1:
 # Build image and add a descriptive tag
--- bin/upload_docker.sh
+++ bin/upload_docker.sh
@@ -4,7 +4,7 @@
 # Assumes that an image is built via `run_docker.sh`
 DOCKER_HUB_ID="gampie"
 DOCKER_REPOSITORY="hello-app"
-VERSION="1.0"
+VERSION="1.000"
 
 # Step 1:
 # Create dockerpath
@@ -14,7 +14,7 @@ dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}
 # Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-# docker login -u ${DOCKER_HUB_ID}
+docker login -u ${DOCKER_HUB_ID}
 docker tag ${DOCKER_REPOSITORY}:${VERSION} ${dockerpath}:${VERSION}
 
 # Step 3:
